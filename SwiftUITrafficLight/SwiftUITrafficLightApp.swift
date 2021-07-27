717ba0200316087f20fc71ca25df00b1549457de
Yuri Martinovsky <vvrite@me.com>
2021-07-27 14:17:35 +0300

    Initial Commit

---
+++ SwiftUITrafficLight/SwiftUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightApp.swift
+//  SwiftUITrafficLight
+//
+//  Created by Yuriy Martinovskiy on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

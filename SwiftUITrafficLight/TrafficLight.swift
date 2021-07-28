591db970dbd09d8b78c8d7f92081a913fba51fe4
Yuri Martinovsky <vvrite@me.com>
2021-07-28 11:03:12 +0300

    Added code

--- SwiftUITrafficLight/TrafficLight.swift
+++ SwiftUITrafficLight/TrafficLight.swift
@@ -8,18 +8,50 @@
 import SwiftUI
 
 struct TrafficLight: View {
+    enum StateLights {
+        case stop
+        case wait
+        case go
+    }
+    
+    @State var colorRed = Color.red
+    @State var colorYellow = Color.yellow
+    @State var colorGreen = Color.green
+    @State var title = "START"
+    @State var state: StateLights = .stop
+    
     var body: some View {
         VStack {
-            Lights(color: .red)
-            Lights(color: .yellow)
-            Lights(color: .green)
+            Lights(color: colorRed)
+            Lights(color: colorYellow)
+            Lights(color: colorGreen)
             
             Spacer()
             
-            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
-            }.padding(.bottom, 80)
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                title = "NEXT"
+                switch state {
+                case .stop:
+                    colorRed = .red
+                    colorYellow = .gray
+                    colorGreen = .gray
+                    state = .wait
+                case .wait:
+                    colorRed = .gray
+                    colorYellow = .yellow
+                    colorGreen = .gray
+                    state = .go
+                case .go:
+                    colorRed = .gray
+                    colorYellow = .gray
+                    colorGreen = .green
+                    state = .stop
+                }
+            }) {
+                Text(title)
+            }.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 30, alignment: .center)
+             .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+            Spacer()
 
         }
     }
